ff070aad4540123d29d984199d4277c5a371b370
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2025-02-21 13:43:42 +0700

    Zlib Encode

--- machinedata/Encode.cs
+++ machinedata/Encode.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace machinedata
 {
@@ -15,15 +13,38 @@ namespace machinedata
 
             using (MemoryStream memoryStream = new MemoryStream())
             {
+                // เขียน zlib header
+                memoryStream.WriteByte(0x78);
+                memoryStream.WriteByte(0x9C);
+
                 using (DeflateStream deflateStream = new DeflateStream(memoryStream, CompressionLevel.Optimal, true))
                 {
                     deflateStream.Write(byteArray, 0, byteArray.Length);
                 }
 
-                byte[] compressedBytes = memoryStream.ToArray();
+                // คำนวณ Adler-32 checksum และเพิ่มลงท้าย
+                uint adler32 = ComputeAdler32(byteArray);
+                byte[] adlerBytes = BitConverter.GetBytes(adler32);
+                Array.Reverse(adlerBytes); // ให้เป็น Big-Endian
+                memoryStream.Write(adlerBytes, 0, adlerBytes.Length);
 
+                byte[] compressedBytes = memoryStream.ToArray();
                 return Convert.ToBase64String(compressedBytes);
             }
+        }
+
+        private static uint ComputeAdler32(byte[] data)
+        {
+            const uint MOD_ADLER = 65521;
+            uint a = 1, b = 0;
+
+            foreach (byte value in data)
+            {
+                a = (a + value) % MOD_ADLER;
+                b = (b + a) % MOD_ADLER;
+            }
+
+            return (b << 16) | a;
         }
     }
 }
